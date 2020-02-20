13b5f5c5b10ae78534f6c5fe565643996504a064
Jaschbinhacht <janniknoeldner@googlemail.com>
2020-02-20 14:23:20 +0100

    Commit changed compiler version

--- src/helm/HelmDeployer.java
+++ src/helm/HelmDeployer.java
@@ -26,13 +26,31 @@ import org.microbean.helm.chart.URLChartLoader;
 
 public class HelmDeployer {
     public void testDeploy() throws Exception {
+
         final URI uri = URI.create("https://kubernetes-charts.storage.googleapis.com/wordpress-0.6.6.tgz");
         assert uri != null;
         final URL url = uri.toURL();
         assert url != null;
         Chart.Builder chart = null;
-        DefaultKubernetesClient kub = new DefaultKubernetesClient();
-        InstallReleaseRequest req = InstallReleaseRequest.newBuilder().build();
+        try (final URLChartLoader chartLoader = new URLChartLoader()) {
+            chart = chartLoader.load(url);
+        }
+        assert chart != null;
 
+        try (final DefaultKubernetesClient client = new DefaultKubernetesClient();
+             final Tiller tiller = new Tiller(client);
+             final ReleaseManager releaseManager = new ReleaseManager(tiller)) {
+
+            final InstallReleaseRequest.Builder requestBuilder = InstallReleaseRequest.newBuilder();
+            assert requestBuilder != null;
+            requestBuilder.setTimeout(300L);
+            requestBuilder.setName("test-charts"); // Set the Helm release name
+            requestBuilder.setWait(true); // Wait for Pods to be ready
+
+            final Future<InstallReleaseResponse> releaseFuture = releaseManager.install(requestBuilder, chart);
+            assert releaseFuture != null;
+            final Release release = releaseFuture.get().getRelease();
+            assert release != null;
+        }
     }
 }
