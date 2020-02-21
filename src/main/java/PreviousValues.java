4816ce13a6f57af6d38b5f3fbd516587dcf690dc
JrMick <jrruck@googlemail.com>
2020-02-21 17:24:13 +0100

    Minor changes.

--- src/main/java/PreviousValues.java
+++ src/main/java/PreviousValues.java
@@ -1,7 +1,7 @@
 public class PreviousValues {
     String service_id;
     String plan_id;
-    String organization_id:
+    String organization_id;
     String space_id;
-    //MaintenanceInfo maintenance_info
+    MaintenanceInfo maintenance_info;
 }
