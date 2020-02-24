ad3e53ee3bf1b68bca47fc6fa477b0705d62a44d
JrMick <jrruck@googlemail.com>
2020-02-24 21:52:03 +0100

    ServiceCatalog added.

---
+++ model/src/main/java/de/thbin/epro/model/ServiceCatalog.java
@@ -0,0 +1,25 @@
+package src.main.java.de.thbin.epro.model;
+
+public class ServiceCatalog {
+
+    // must have
+
+    private ServiceOffering[] services;
+
+    // CONSTRUCTOR
+
+    public ServiceCatalog(){
+
+    }
+
+    // GETTER AND SETTER
+
+
+    public ServiceOffering[] getServices() {
+        return services;
+    }
+
+    public void setServices(ServiceOffering[] services) {
+        this.services = services;
+    }
+}
