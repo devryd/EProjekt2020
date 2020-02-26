6b8f55e04ef4bab3c8856be1ef4468e0d247c32b
MaxFuhrich <fuhrich@bahnhof-ebschied.de>
2020-02-26 16:34:18 +0100

    Added classes for responses. Added responses in ServiceBrokerImpl.java TO DO Missing responses, Calling the methods for creating/deleting instances/bindings etc

--- model/src/main/java/de/thbin/epro/model/PollBody.java
+++ model/src/main/java/de/thbin/epro/model/PollBody.java
@@ -1,4 +1,40 @@
 package de.thbin.epro.model;
 
 public class PollBody {
+    String state;
+    String description;
+    boolean instance_usable;
+    boolean update_repeatable;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isInstance_usable() {
+        return instance_usable;
+    }
+
+    public void setInstance_usable(boolean instance_usable) {
+        this.instance_usable = instance_usable;
+    }
+
+    public boolean isUpdate_repeatable() {
+        return update_repeatable;
+    }
+
+    public void setUpdate_repeatable(boolean update_repeatable) {
+        this.update_repeatable = update_repeatable;
+    }
 }
